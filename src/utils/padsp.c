200324b0afbfe8dc11636ccc697420e1166e4c29
Aurelien Larcher <aurelien.larcher@gmail.com>
2015-12-07 00:51:46 +0100

    02-padsp.patch

--- src/utils/padsp.c
+++ src/utils/padsp.c
@@ -2278,6 +2278,7 @@ static int dsp_ioctl(fd_info *i, unsigned long request, void*argp, int *_errno) 
             break;
         }
 
+#ifndef __sun
         case SOUND_PCM_READ_RATE:
             debug(DEBUG_LEVEL_NORMAL, __FILE__": SOUND_PCM_READ_RATE\n");
 
@@ -2301,6 +2302,7 @@ static int dsp_ioctl(fd_info *i, unsigned long request, void*argp, int *_errno) 
             *(int*) argp = pa_sample_size(&i->sample_spec)*8;
             pa_threaded_mainloop_unlock(i->mainloop);
             break;
+#endif
 
         case SNDCTL_DSP_GETOPTR: {
             count_info *info;
@@ -2371,7 +2373,7 @@ fail:
     return ret;
 }
 
-#ifdef sun
+#ifdef __sun
 int ioctl(int fd, int request, ...) {
 #else
 int ioctl(int fd, unsigned long request, ...) {
