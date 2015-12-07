97c282b1a5422bfb6d86f7ab7294e4536a9b6dd6
Aurelien Larcher <aurelien.larcher@gmail.com>
2015-12-07 01:35:54 +0100

    Define __WORDSIZE (shawn.walker@oracle.com)

--- src/pulsecore/sample-util.h
+++ src/pulsecore/sample-util.h
@@ -32,6 +32,18 @@
 #include <pulsecore/memblock.h>
 #include <pulsecore/memchunk.h>
 
+/*
+ * __WORDSIZE is a non-portable glibc-specific internal macro.
+ * https://bugs.freedesktop.org/show_bug.cgi?id=90880
+ */
+#if !defined(__WORDSIZE)
+# if defined(__LP64__)
+#  define __WORDSIZE	64
+# else
+#  define __WORDSIZE	32
+# endif
+#endif
+
 typedef struct pa_silence_cache {
     pa_memblock* blocks[PA_SAMPLE_MAX];
 } pa_silence_cache;
